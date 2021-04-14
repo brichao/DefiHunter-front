80cb03205a3fd44679fe55dab6be5ee11a5a4789
l3miage-matteiv <77281307+l3miage-matteiv@users.noreply.github.com>
2021-04-14 15:03:35 +0200

    Authorisation, Connexion et deconnexion fonctionne avec affichage du nom de l'utilisateur

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
+import { HttpClient, HttpParams, HttpResponse } from '@angular/common/http';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { OSM_TILE_LAYER_URL } from '@yaga/leaflet-ng2';
-import firebase from 'firebase/app'
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -13,19 +14,35 @@ export class AppComponent {
   dataIconGoogle = 'assets/images/iconGoogle.png';
   iconMarker = 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Map_marker.svg/585px-Map_marker.svg.png';
   tileLayerUrl = OSM_TILE_LAYER_URL;
-  constructor(public auth: AngularFireAuth) { }
+
+  chamis: firebase.default.User | null = null;
+
+  constructor(public auth: AngularFireAuth) {
+    auth.authState
+      .subscribe(chamis => {
+        if (chamis) {
+          this.chamis = chamis;
+          console.log(this.chamis);
+        }
+        else {
+          this.chamis = null;
+        }
+      })
+  }
 
   login(): void {
-    const provider = new firebase.auth.GoogleAuthProvider();
+    const provider = new firebase.default.auth.GoogleAuthProvider();
     provider.setCustomParameters({
       prompt: 'select_account'
     });
     this.auth.signInWithPopup(provider);
-    // this.auth.user
-    //   .subscribe
   }
 
   logout(): void {
     this.auth.signOut();
   }
+
+  isLoggedIn() {
+    return this.chamis != null;
+  }
 }
