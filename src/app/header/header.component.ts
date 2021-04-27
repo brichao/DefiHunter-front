3815191c4b1486317103c92a6c5b3b711dd71827
l3miage-matteiv <77281307+l3miage-matteiv@users.noreply.github.com>
2021-04-27 16:56:59 +0200

    Tableaux mises a droite

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -11,13 +11,12 @@ import { Observable } from 'rxjs';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HeaderComponent {
-
-  @Output()
-  public menuEvent = new EventEmitter<MouseEvent>();
-
   dataIconGoogle = 'assets/images/iconGoogle.png';
   iconMarker = 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Map_marker.svg/585px-Map_marker.svg.png';
   tileLayerUrl = OSM_TILE_LAYER_URL;
+
+  @Output()
+  public menuEvent = new EventEmitter<MouseEvent>();
 
   chamis$: Observable<firebase.User | null>;
 
@@ -41,11 +40,7 @@ export class HeaderComponent {
     this.auth.signOut();
   }
 
-  isLoggedIn(): boolean {
-    return this.chamis$ != null;
-  }
-
-  addUser(): void {
-    // ...
+  isLoggedIn(): Observable<firebase.User | null> {
+    return this.chamis$;
   }
 }
