84608eb6f5485d1f27a6737044727c3712551e9b
l3miage-matteiv <77281307+l3miage-matteiv@users.noreply.github.com>
2021-04-28 12:36:25 +0200

    Petites modifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,33 +11,6 @@ import { Observable } from 'rxjs';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-  dataIconGoogle = 'assets/images/iconGoogle.png';
   iconMarker = 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Map_marker.svg/585px-Map_marker.svg.png';
   tileLayerUrl = OSM_TILE_LAYER_URL;
-
-  chamis$: Observable<firebase.User | null>;
-
-  constructor(public auth: AngularFireAuth) {
-    this.chamis$ = auth.authState;
-  }
-
-  login(): void {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    provider.setCustomParameters({
-      prompt: 'select_account'
-    });
-    this.auth.signInWithPopup(provider);
-  }
-
-  logout(): void {
-    this.auth.signOut();
-  }
-
-  isLoggedIn(): boolean {
-    return this.chamis$ != null;
-  }
-
-  addUser(): void {
-    // ...
-  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -12,8 +12,6 @@ import { Observable } from 'rxjs';
 })
 export class HeaderComponent {
   dataIconGoogle = 'assets/images/iconGoogle.png';
-  iconMarker = 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Map_marker.svg/585px-Map_marker.svg.png';
-  tileLayerUrl = OSM_TILE_LAYER_URL;
 
   @Output()
   public menuEvent = new EventEmitter<MouseEvent>();
